38ca7c90a23f6bbfd172442374af8dc0fb497e11
Hussyvel <hussyribeiro@hotmail.com>
2019-11-04 13:22:19 -0300

    Programs basics

---
+++ TestaEscopo2.java
@@ -0,0 +1,23 @@
+
+
+public class TestaEscopo2 {
+		public static void main(String[] args) {
+				System.out.println("Testando condicionais");
+				
+				int idade = 20;
+				int quantidadePessoas = 3;
+					
+				boolean acompanhado;
+				
+				if (quantidadePessoas >= 2) {
+					acompanhado = true;
+				
+				if (idade >= 18 || acompanhado == true) {
+				
+					System.out.println("Seja bem vindo");
+				} else {
+					System.out.println("Infelizmente você não pode entrar");
+			}
+		}
+	}
+}
